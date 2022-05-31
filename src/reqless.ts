f7b5007d63b0a14e060c0e655d7503f962611dae
Holly <holly@dejawu.me>
2022-05-31 15:46:08 -0400

    Visit all deps

--- src/reqless.ts
+++ src/reqless.ts
@@ -6,53 +6,87 @@ import {
 } from "@swc/core";
 import { Visitor } from "@swc/core/Visitor.js";
 import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
 import { createRequire } from "node:module";
 import { match } from "ts-pattern";
 
-const require = createRequire(import.meta.url);
-
-class CollectDeps extends Visitor {
-  visitImportDeclaration(n: ImportDeclaration): ImportDeclaration {
-    return n;
-  }
-
-  visitCallExpression(exp: CallExpression): Expression {
-    // TODO make sure "require" is never re-defined in the file
-
-    const { callee, arguments: args } = exp;
-
-    if (callee.type !== "Identifier") {
-      return exp;
-    }
-
-    if (args.length !== 1) {
-      console.warn("Warning: require called without parameter");
-      return exp;
-    }
-
-    const { expression: arg } = args[0];
-
-    if (arg.type !== "StringLiteral") {
-      return exp;
-    }
-
-    const path = arg.value;
-
-    console.log(`Requiring ${path}`);
-
-    return exp;
-  }
-}
-
-const entryPoint = process.argv[2];
+const entryPoint = resolve(process.argv[2]);
 if (!entryPoint) {
   throw new Error("No entrypoint specified");
 }
 
-const file = readFileSync(entryPoint).toString();
+type Dep = {
+  kind: "import" | "require";
+  absPath: string;
+  visited: boolean;
+};
 
-const deps = [];
+const deps: Dep[] = [];
 
-const out = transformSync(file, {
-  plugin: (m) => new CollectDeps().visitProgram(m),
-});
+// collect a module's dependencies and return a transformed version of the module
+const visitModule = (absPath): string => {
+  const require = createRequire(absPath);
+  class CollectDeps extends Visitor {
+    visitImportDeclaration(n: ImportDeclaration): ImportDeclaration {
+      return n;
+    }
+
+    visitCallExpression(exp: CallExpression): Expression {
+      // TODO make sure "require" is never re-defined in the file
+      const { callee, arguments: args } = exp;
+
+      if (callee.type !== "Identifier") {
+        return exp;
+      }
+
+      if (args.length !== 1) {
+        console.warn("Warning: require called without parameter");
+        return exp;
+      }
+
+      const { expression: arg } = args[0];
+
+      if (arg.type !== "StringLiteral") {
+        return exp;
+      }
+
+      const { value: path } = arg;
+
+      const depAbsPath = require.resolve(path);
+
+      if (!deps.some(({ absPath }) => absPath === depAbsPath)) {
+        deps.push({
+          kind: "require",
+          absPath: require.resolve(path),
+          visited: false,
+        });
+      }
+
+      return exp;
+    }
+  }
+
+  const file = readFileSync(absPath).toString();
+
+  return transformSync(file, {
+    plugin: (m) => new CollectDeps().visitProgram(m),
+  }).code;
+};
+
+// seed deps with entrypoint deps
+visitModule(entryPoint);
+
+while (true) {
+  const unvisitedDeps = deps.filter(({ visited }) => !visited);
+
+  if (unvisitedDeps.length === 0) {
+    break;
+  }
+
+  unvisitedDeps.forEach((dep) => {
+    visitModule(dep.absPath);
+    dep.visited = true;
+  });
+}
+
+console.log(deps);
